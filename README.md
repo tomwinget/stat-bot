769c1686c70c7b3d5a291c820bd755e2626c8726
Tom <tomwinget@gmail.com>
2019-10-08 10:50:27 -0700

    Initialize stat-bot

---
+++ README.md
@@ -0,0 +1,5 @@
+# stat-bot
+Python/Redis based stat bot for discord
+
+bot commands for calculating reaction usage in a server
+backed by Redis for storing calculations
---
+++ stat-bot.py
@@ -0,0 +1,201 @@
+import os
+import random
+import discord
+import redis
+from time import localtime, strftime
+from discord import VoiceChannel
+from discord import CategoryChannel
+from discord import DMChannel
+from discord.ext import commands
+from discord.ext.commands import has_role
+from operator import itemgetter
+
+token = os.getenv('statToken')
+
+excluded_channel_types = [VoiceChannel, CategoryChannel, DMChannel]
+
+bot = commands.Bot(command_prefix='~')
+
+r_local = redis.StrictRedis(decode_responses=True)
+r_local.set("startup", strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+
+list_msg_format = "* %s: %d\n"
+
+@bot.event
+async def on_ready():
+    print("Stat-bot ready!")
+    print("Script started: %s" % r_local.get("startup"))
+
+
+@bot.command(name='random-user')
+async def random_user(ctx):
+    with ctx.message.channel.typing():
+        print("Picking random user from channel: %s" % ctx.message.channel)
+        await ctx.send(random.choice(ctx.message.channel.members))
+
+@bot.command(name='random-emote')
+async def random_emote(ctx):
+    with ctx.message.channel.typing():
+        print("Picking random emote!")
+        await ctx.send(random.choice(ctx.guild.emojis))
+
+@bot.command(name='get-stats')
+async def get_stats(ctx, user=None):
+    with ctx.message.channel.typing():
+        if len(ctx.message.mentions) == 0:
+            print("Getting global stats")
+            result = r_local.hgetall("emotes")
+            result = dict([key, int(val)] for key, val in result.items())
+            msg = "```"
+            for emote, count in sorted(result.items(), key=itemgetter(1), reverse=True):
+                msg += list_msg_format % (emote, int(count))
+            msg += "```"
+            print("Result msg: ", msg)
+            await ctx.send(msg)
+        else:
+            for member in ctx.message.mentions:
+                print("Getting member stats for: ", member.id)
+                result = r_local.hgetall(member.id)
+                result = dict([key, int(val)] for key, val in result.items())
+                msg = "```"
+                for emote, count in sorted(result.items(), key=itemgetter(1), reverse=True):
+                    msg += list_msg_format % (emote, int(count))
+                msg += "```"
+                print("Result msg: ", msg)
+                await ctx.send(msg)
+
+@bot.command(name='store-stats')
+@has_role("Bot Admin")
+async def store_stats(ctx):
+    async with ctx.message.channel.typing():
+        print("Save db to disk")
+        await ctx.send("Saving current db image to disk")
+        r_local.save()
+        print("Flushing db")
+        await ctx.send("Flushing db")
+        r_local.flushall()
+        for channel in ctx.guild.channels:
+            if hasattr(channel, 'history'):
+                print("Processing messages in channel: %s" % channel)
+                await ctx.send("Processing messages in channel: %s" % channel)
+                most_recent_msg = None
+                async for message in channel.history(limit=None):
+                    if not most_recent_msg:
+                        most_recent_msg = message
+                    elif message.created_at > most_recent_msg.created_at:
+                        most_recent_msg = message
+                    for reaction in message.reactions:
+                        if reaction.custom_emoji:
+                            users = await reaction.users().flatten()
+                            count = 0
+                            for user in users:
+                                if not user.bot:
+                                    count += 1
+                                    r_local.hincrby(user.id, reaction.emoji.name)
+                            if count != 0:
+                                r_local.hincrby("emotes", reaction.emoji.name, count)
+                print("Most recent message id: %d" % most_recent_msg.id)
+                r_local.hset("most_recent", channel.id, most_recent_msg.id)
+    await ctx.send("Processed all messages!")
+
+@bot.command(name='add-stats')
+async def add_stats(ctx):
+    async with ctx.message.channel.typing():
+        for channel in ctx.guild.channels:
+            if hasattr(channel, 'history'):
+                last_update_id = r_local.hget("most_recent",channel.id)
+                print("Got ID: %s"%last_update_id)
+                last_message = None
+                if last_update_id:
+                    last_message = await channel.fetch_message(int(last_update_id))
+                await ctx.send("Processing messages in channel: %s for message: %s" % (channel, last_message.id))
+                if last_message:
+                    most_recent_message = None
+                    async for message in channel.history(limit=None,after=last_message):
+                        if not most_recent_message:
+                            most_recent_message = message
+                        elif message.created_at > most_recent_message.created_at:
+                            most_recent_message = message
+                        for reaction in message.reactions:
+                            if reaction.custom_emoji:
+                                users = await reaction.users().flatten()
+                                count = 0
+                                for user in users:
+                                    if not user.bot:
+                                        count += 1
+                                        r_local.hincrby(user.id, reaction.emoji.name)
+                                if count != 0:
+                                    r_local.hincrby("emotes", reaction.emoji.name, count)
+                    if most_recent_message:
+                        print("Most recent message id: %d" % most_recent_message.id)
+                        r_local.hset("most_recent", channel.id, most_recent_message.id)
+                else:
+                    most_recent_message = None
+                    async for message in channel.history(limit=None):
+                        if not most_recent_message:
+                            most_recent_message = message
+                        elif message.created_at > most_recent_message.created_at:
+                            most_recent_message = message
+                        for reaction in message.reactions:
+                            if reaction.custom_emoji:
+                                users = await reaction.users().flatten()
+                                count = 0
+                                for user in users:
+                                    if not user.bot:
+                                        count += 1
+                                        r_local.hincrby(user.id, reaction.emoji.name)
+                                if count != 0:
+                                    r_local.hincrby("emotes", reaction.emoji.name, count)
+                    if most_recent_message:
+                        print("Most recent message id: %d" % most_recent_message.id)
+                        r_local.hset("most_recent", channel.id, most_recent_message.id)
+    await ctx.send("Processed all messages!")
+                
+
+@random_user.error
+@random_emote.error
+@get_stats.error
+@add_stats.error
+@store_stats.error
+async def store_stats_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have permission for this command!")
+    elif isinstance(error, commands.MissingRole):
+        await ctx.send("You aren't the proper role for this command!")
+    else:
+        await ctx.send("Error occurred processing your request : %s"%str(error))
+
+@bot.command(name='calc-stats')
+async def calc_stats(ctx, msg_limit=1000, response_size=50):
+    async with ctx.message.channel.typing():
+        print("Starting reaction calculation")
+        dict_of_reacts = {}
+        for channel in ctx.guild.channels:
+            if type(channel) not in excluded_channel_types:
+                print("Processing messages for channel: %s" % channel)
+                async for message in channel.history(limit=msg_limit):
+                    for reaction in message.reactions:
+                        if reaction.custom_emoji:
+                            users = await reaction.users().flatten()
+                            count = 0
+                            for user in users:
+                                if not user.bot:
+                                    count += 1
+                            if count != 0:
+                                dict_of_reacts[reaction.emoji.name] = dict_of_reacts.get(reaction.emoji.name, 0) + count
+
+        list_size = 0
+        mssage = "```"
+        for emote, count in sorted(dict_of_reacts.items(), key=itemgetter(1), reverse=True):
+            mssage += list_msg_format % (emote, count)
+            list_size += 1
+            if list_size > response_size:
+                mssage += "```"
+                await ctx.send(mssage)
+                list_size = 0
+                mssage = "```"
+
+        await ctx.send(mssage+"```")
+    print("Completed stats calculation!")
+            
+bot.run(token)
